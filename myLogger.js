d112b5ba99883b61c8655d42d6c840d565c0a925
Alvis Hsu <junchoon14@gmail.com>
2019-05-23 23:11:10 +0800

    Update myLogger.js

--- myLogger.js
+++ myLogger.js
@@ -1,7 +1,7 @@
 module.exports = logger = (options) => {
   return logger = (req, res, next) => {
     let date = new Date()
-    console.log(date.toLocaleString(), req.method, req.url)
+    console.log(date.toLocaleString(), '|', req.method, 'from', req.url)
     next()
   }
 }
